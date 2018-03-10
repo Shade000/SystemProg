7c4f060e84496f1f5e151bca78db3724fb46b149
Shade000 <2543631@mail.ru>
2018-03-10 20:25:51 +0300

    HomeWork_3

---
+++ HomeWork_3/Sinhronizacia/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Sinhronizacia_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new List<int>();
+            var r = new List<int>();
+
+            var a = new Thread(() =>
+            {
+                for (int i = 1; i < 20; i++)
+                {
+                    Console.WriteLine("Поток A добавляет в список S число {0}", i);
+                    s.Add(i);
+                }
+            });
+
+            var b = new Thread(() =>
+            {
+                Console.WriteLine("Поток B извлекает из списка S последний элемент, возводит его в квадрат и помещает в список R");
+                if (s.Count == 0)
+                    Thread.Sleep(1000);
+                r.Add(s.Last() * s.Last());
+            });
+
+            var c = new Thread(() =>
+            {
+                Console.WriteLine("Поток C извлекает из списка S последний элемент, делит его на 3 и помещает в список R");
+                if (s.Count != 0)
+                    r.Add(s.Last() / 3);
+            });
+
+            var d = new Thread(() =>
+            {
+                Console.WriteLine("Поток D извлекает из списка R последний элемент и печатает его");
+                if (r.Count == 0)
+                {
+                    Console.WriteLine("В списке R нет элементов");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("Последний элемент из списка R = {0}", r.Last());
+            });
+            new Thread(() =>
+            {
+                a.Start();
+                b.Start();
+                c.Start();
+                d.Start();
+            }).Start();
+          
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork_3/Sinhronizacia_Task_2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Sinhronizacia_Task_2
+{
+    class Program
+    {
+        private static readonly object _locker_S = new object();
+        private static readonly object _locker_R = new object();
+        static void Main(string[] args)
+        {
+            var s = new List<int>();
+            var r = new List<int>();
+
+            var a = new Thread(() =>
+            {
+                while (true)
+                {
+                    lock (_locker_S)                        
+                    {
+                        for (int i = 1; i < 5; i++)
+                        {
+                            Console.WriteLine("Поток A добавляет в список S число {0}", i);
+                            s.Add(i);
+                        }
+                    }
+                }
+            });
+
+            var b = new Thread(() =>
+            {
+                int x = 0;
+                while (true)
+                {
+                    if (s.Count == 0)
+                        Thread.Sleep(1000);
+                    else
+                    {
+                        lock (_locker_S)
+                        {
+                            Thread.Sleep(1500);
+                            lock (_locker_R)
+                            {
+                                Console.WriteLine("Поток B извлекает из списка S последний элемент, возводит его в квадрат и помещает в список R");
+                                x = s.Last();
+                                r.Add(x * x);
+                            }
+                        }
+                    }
+                }
+            });
+
+            //Взаимоблокировка
+            //var b = new Thread(() =>
+            //{
+            //    while (true)
+            //    {
+            //        int x = 0;
+            //        lock (_locker_S)
+            //        {
+            //            if (s.Count == 0)
+            //            {
+            //                Thread.Sleep(1000);
+            //            }
+            //            x = s.Last();
+            //            lock (_locker_R)
+            //            {
+            //                Console.WriteLine("Поток B извлекает из списка S последний элемент, возводит его в квадрат и помещает в список R");
+            //                Thread.Sleep(3000);
+            //                r.Add(x * x);
+            //            }
+            //        }
+            //        lock (_locker_R)
+            //        {
+            //            lock (_locker_S)
+            //            {
+            //                Thread.Sleep(3000);
+            //                Console.WriteLine("Последний элемент из списка S = {0}", s.Count);
+            //            }
+            //        }
+            //    }     
+            //});
+
+            var c = new Thread(() =>
+            {
+                int x = 0;
+                while (true)
+                {
+                    if (s.Count == 0)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        lock (_locker_R)
+                        {
+                            Thread.Sleep(1500);
+                            lock (_locker_S)
+                            {
+                                Console.WriteLine("Поток C извлекает из списка S последний элемент, делит его на 3 и помещает в список R");
+                                x = s.Last();
+                                r.Add(x / 3);
+                            }
+                        }
+                    }
+                }
+            });
+
+            var d = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (r.Count == 0)
+                    {
+                        Console.WriteLine("В списке R нет элементов");
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        lock (_locker_R)
+                        {
+                            Console.WriteLine(r.Last());
+                        }
+                    }
+                }
+            });
+
+            new Thread(() =>
+            {
+                a.Start();
+                b.Start();
+                c.Start();
+                d.Start();
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
